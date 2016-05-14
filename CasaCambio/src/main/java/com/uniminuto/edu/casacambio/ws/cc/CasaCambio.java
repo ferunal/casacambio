9ee8745b0f6b769d681c24cd68eaaeb657fb97df
Luis Fernando Chacón <luisferchacon.lfc@gmail.com>
2016-05-14 16:57:40 -0500

    ProyectoInicial

---
+++ CasaCambio/src/main/java/com/uniminuto/edu/casacambio/ws/cc/CasaCambio.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uniminuto.edu.casacambio.ws.cc;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author fercris
+ */
+@WebService(serviceName = "CasaCambio")
+@Stateless()
+public class CasaCambio {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    @WebMethod(operationName = "cambiaDolaresASoles")
+    public double cambiaDolaresASoles(@WebParam(name = "monto") double monto) {
+        return monto * 3.6;
+    }
+
+    @WebMethod(operationName = "cambiarSolesADolares")
+    public double cambiarSolesADolares(@WebParam(name = "monto") double monto) {
+        if (monto != 0d) {
+            return 3.6 / monto;
+        } else {
+            return 0d;
+        }
+
+    }
+}
